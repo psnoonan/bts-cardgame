b1bfbb4d28dc5d25f1c7159d50120acd30190995
Patrick Noonan <psnoonan1980@gmail.com>
2020-04-25 16:10:48 -0700

    Add card shuffling and dealing

---
+++ src/helpers/deck-seed.js
@@ -0,0 +1,271 @@
+const hearts = [
+    {
+        value: 1,
+        suit: 'hearts',
+        label: 'ace',
+    },
+    {
+        value: 2,
+        suit: 'hearts',
+        label: 'two',
+    },
+    {
+        value: 3,
+        suit: 'hearts',
+        label: 'three',
+    },
+    {
+        value: 4,
+        suit: 'hearts',
+        label: 'four',
+    },
+    {
+        value: 5,
+        suit: 'hearts',
+        label: 'five',
+    },
+    {
+        value: 6,
+        suit: 'hearts',
+        label: 'six',
+    },
+    {
+        value: 7,
+        suit: 'hearts',
+        label: 'seven',
+    },
+    {
+        value: 8,
+        suit: 'hearts',
+        label: 'eight',
+    },
+    {
+        value: 9,
+        suit: 'hearts',
+        label: 'nine',
+    },
+    {
+        value: 10,
+        suit: 'hearts',
+        label: 'ten',
+    },
+    {
+        value: 11,
+        suit: 'hearts',
+        label: 'jack',
+    },
+    {
+        value: 12,
+        suit: 'hearts',
+        label: 'queen',
+    },
+    {
+        value: 13,
+        suit: 'hearts',
+        label: 'king',
+    },
+];
+
+const diamonds = [
+    {
+        value: 1,
+        suit: 'diamonds',
+        label: 'ace',
+    },
+    {
+        value: 2,
+        suit: 'diamonds',
+        label: 'two',
+    },
+    {
+        value: 3,
+        suit: 'diamonds',
+        label: 'three',
+    },
+    {
+        value: 4,
+        suit: 'diamonds',
+        label: 'four',
+    },
+    {
+        value: 5,
+        suit: 'diamonds',
+        label: 'five',
+    },
+    {
+        value: 6,
+        suit: 'diamonds',
+        label: 'six',
+    },
+    {
+        value: 7,
+        suit: 'diamonds',
+        label: 'seven',
+    },
+    {
+        value: 8,
+        suit: 'diamonds',
+        label: 'eight',
+    },
+    {
+        value: 9,
+        suit: 'diamonds',
+        label: 'nine',
+    },
+    {
+        value: 10,
+        suit: 'diamonds',
+        label: 'ten',
+    },
+    {
+        value: 11,
+        suit: 'diamonds',
+        label: 'jack',
+    },
+    {
+        value: 12,
+        suit: 'diamonds',
+        label: 'queen',
+    },
+    {
+        value: 13,
+        suit: 'diamonds',
+        label: 'king',
+    },
+];
+const clubs = [
+    {
+        value: 1,
+        suit: 'clubs',
+        label: 'ace',
+    },
+    {
+        value: 2,
+        suit: 'clubs',
+        label: 'two',
+    },
+    {
+        value: 3,
+        suit: 'clubs',
+        label: 'three',
+    },
+    {
+        value: 4,
+        suit: 'clubs',
+        label: 'four',
+    },
+    {
+        value: 5,
+        suit: 'clubs',
+        label: 'five',
+    },
+    {
+        value: 6,
+        suit: 'clubs',
+        label: 'six',
+    },
+    {
+        value: 7,
+        suit: 'clubs',
+        label: 'seven',
+    },
+    {
+        value: 8,
+        suit: 'clubs',
+        label: 'eight',
+    },
+    {
+        value: 9,
+        suit: 'clubs',
+        label: 'nine',
+    },
+    {
+        value: 10,
+        suit: 'clubs',
+        label: 'ten',
+    },
+    {
+        value: 11,
+        suit: 'clubs',
+        label: 'jack',
+    },
+    {
+        value: 12,
+        suit: 'clubs',
+        label: 'queen',
+    },
+    {
+        value: 13,
+        suit: 'clubs',
+        label: 'king',
+    },
+];
+const spades = [
+    {
+        value: 1,
+        suit: 'spades',
+        label: 'ace',
+    },
+    {
+        value: 2,
+        suit: 'spades',
+        label: 'two',
+    },
+    {
+        value: 3,
+        suit: 'spades',
+        label: 'three',
+    },
+    {
+        value: 4,
+        suit: 'spades',
+        label: 'four',
+    },
+    {
+        value: 5,
+        suit: 'spades',
+        label: 'five',
+    },
+    {
+        value: 6,
+        suit: 'spades',
+        label: 'six',
+    },
+    {
+        value: 7,
+        suit: 'spades',
+        label: 'seven',
+    },
+    {
+        value: 8,
+        suit: 'spades',
+        label: 'eight',
+    },
+    {
+        value: 9,
+        suit: 'spades',
+        label: 'nine',
+    },
+    {
+        value: 10,
+        suit: 'spades',
+        label: 'ten',
+    },
+    {
+        value: 11,
+        suit: 'spades',
+        label: 'jack',
+    },
+    {
+        value: 12,
+        suit: 'spades',
+        label: 'queen',
+    },
+    {
+        value: 13,
+        suit: 'spades',
+        label: 'king',
+    },
+];
+
+export default [...hearts, ...diamonds, ...clubs, ...spades];
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,92 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
-  </div>
+    <div class="home">
+        <h1>BTS Card Game Proof of Concept</h1>
+
+        <div>Cards Remaining: {{ deckLive.length }}</div>
+        <div>Used Cards: {{ deckUsed.length }}</div>
+
+        <button @click="shuffle">Shuffle</button>
+
+        <button @click="dealCard">Deal Card</button>
+
+        <div v-if="firstCard">
+            <span>First Card: </span>
+            <span>{{ firstCard.label }} of {{ firstCard.suit }} </span>
+            <span>({{ firstCard.value }})</span>
+        </div>
+
+        <div v-if="secondCard">
+            <span>Second Card: </span>
+            <span>{{ secondCard.label }} of {{ secondCard.suit }} </span>
+            <span>({{ secondCard.value }})</span>
+        </div>
+    </div>
 </template>
+
+<script>
+import deckSeed from '@/helpers/deck-seed';
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+            deckLive: [],
+            deckUsed: [],
+            firstCard: null,
+            middleCard: null,
+            secondCard: null,
+        };
+    },
+    created() {
+        this.deckUsed = deckSeed;
+    },
+    methods: {
+        shuffle() {
+            // assign used deck to variable for typing ease
+            const arr = this.deckUsed;
+            for (let i = arr.length - 1; i > 0; i--) {
+                // Set j to random index from 0 to i
+                let j = Math.floor(Math.random() * (i + 1));
+                // swap element array[i] and array [j] with destructuring
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+            // add shuffled cards to end of live deck
+            this.deckLive.push(...arr);
+            // reset used deck
+            this.deckUsed = [];
+        },
+        dealCard() {
+            if (this.deckLive.length > 3) {
+                if (!this.firstCard) {
+                    this.firstCard = this.getRandomCard();
+                } else if (!this.secondCard) {
+                    this.secondCard = this.getRandomCard();
+                }
+            }
+        },
+        getRandomCard() {
+            if (this.deckLive.length) {
+                // max is the highest index in live deck
+                // min is always zero, so it is removed from equation
+                const max = this.deckLive.length - 1;
+                // get random index from the live deck
+                const index = Math.floor(Math.random() * (max + 1));
+                // remove card from live deck and store it
+                const [card] = this.deckLive.splice(index, 1);
+                // put card in used deck
+                this.deckUsed.push(card);
+                // prompt for high or low if an ace
+                if (card.value === 1) {
+                    console.log('before prompt');
+                    const shouldSwitchToHigh = confirm(
+                        'Press OK for high, Cancel for low'
+                    );
+                    card.value = shouldSwitchToHigh ? 14 : 1;
+                }
+                console.log('after prompt');
+                return card;
+            }
+        },
+    },
+};
+</script>
