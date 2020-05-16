7f672a88b078d0ce5a882873f3e17d5ad328656f
Patrick Noonan <psnoonan1980@gmail.com>
2020-05-15 23:22:14 -0700

    Add list transition and style to feed

--- src/components/feed.vue
+++ src/components/feed.vue
@@ -1,7 +1,9 @@
 <template>
-    <ul class="feed">
-        <li v-for="(line, index) in feed" :key="index">{{ line }}</li>
-    </ul>
+    <transition-group tag="ul" name="list" class="feed">
+        <li v-for="(line, index) in feed" :key="feed.length - index">
+            {{ line }}
+        </li>
+    </transition-group>
 </template>
 
 <script>
@@ -16,3 +18,31 @@ export default {
     },
 };
 </script>
+
+<style lang="scss" scoped>
+.feed {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    font-size: 1.2rem;
+    li {
+        padding: 0.5rem 1rem;
+        &:nth-child(even) {
+            background-color: #f6f6fc;
+        }
+    }
+}
+
+.list-enter-active,
+.list-leave-active {
+    transition: all 350ms cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.list-move {
+    transition: all 250ms cubic-bezier(0.25, 0.8, 0.25, 1);
+}
+.list-enter,
+.list-leave-to {
+    opacity: 0;
+    transform: translateX(-25%);
+}
+</style>
